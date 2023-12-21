8eea9ec80ab0e222e6c2fbfbd12eee0e6ef51955
rafaelfs98 <rafaelfs98@outlook.com>
2023-12-21 10:06:57 -0300

    #11 Develop front-end features for adding and managing income sources

---
+++ src/Pages/Registers/Fontes Receitas/FontesReceitasActions.tsx
@@ -0,0 +1,47 @@
+import { IconPencil, IconTrash } from "@tabler/icons-react";
+import { useAtom } from "jotai";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Action } from "../../../Components/ListView/ListViewActions";
+import { deleteFonteReceita } from "../../../Services/FonteReceitas";
+import { selectedItemIdAtom } from "../../../atoms/app.atom";
+
+const FontesReceitaActions = () => {
+  const [selectedItemId] = useAtom(selectedItemIdAtom);
+  const navigate = useNavigate();
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+
+  const handleDelete = async () => {
+    if (!confirm("Deseja excluir esta Fonte de Receita?")) {
+      return;
+    }
+
+    setIsDeleting(true);
+    try {
+      await deleteFonteReceita(selectedItemId as string);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+      setIsDeleting(false);
+      alert("Ocorreu um erro ao excluir a Fonte de Receita.");
+    }
+  };
+
+  const actions: Action[] = [
+    {
+      label: "Editar",
+      icon: <IconPencil size={14} />,
+      onClick: () => navigate(`${selectedItemId}/update`),
+    },
+    {
+      label: "Excluir",
+      icon: <IconTrash size={14} />,
+      onClick: () => handleDelete(),
+      disabled: isDeleting,
+    },
+  ];
+
+  return actions;
+};
+
+export default FontesReceitaActions;
